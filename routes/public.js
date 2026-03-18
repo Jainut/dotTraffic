bd7e1d21a6427098d112d5d29d0503ad42c9cc13
Rique <henriquegoncm@gmail.com>
2026-03-18 18:46:56 -0300

    Add some routes and comments on code

--- routes/public.js
+++ routes/public.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import express from 'express' // Importando a biblioteca express
+import { PrismaClient } from '@prisma/client' // Importando a biblioteca prisma
 
-const prisma = new PrismaClient()
-const router = express.Router()
+const prisma = new PrismaClient() // Definindo as funções do prisma como 'prisma' para poder utilizar
+const router = express.Router() // Definindo a função de Router do express como 'router' para poder utilizar
 
-router.post('/registrarTransformador', async (req, res) => {
-    const transformador = req.body
+router.post('/registrarTransformador', async (req, res) => { // Rota de post padrão para registro dos transformadores
+    const transformador = req.body // Variável transformador recebendo a requisição no body
 
-    try {
+    try { // Tenta cadastrar o transformador no db
         if (transformador.modelo == "" || transformador.numeroDeSerie == "" || transformador.statusProducao == "") {
-            return res.status(500).json({message: "Campos inválidos, tente novamente"})
+            return res.status(500).json({message: "Campos inválidos, tente novamente"}) // Mensagem de erro caso não prencha os campos
         } else {
         const newTransformador = await prisma.transformador.create ( {
-        data: {
+        data: { // Mandando as informações para o db
             numeroDeSerie: transformador.numeroDeSerie,
             modelo: transformador.modelo,
             potenciaKva: transformador.potenciaKva,
@@ -21,25 +21,25 @@ router.post('/registrarTransformador', async (req, res) => {
         }
         })
 
-        return res.status(201).json({message:`Sucesso! Transformador registrado`}) 
+        return res.status(201).json({message:`Sucesso! Transformador registrado`}) // Se conseguir registrar no db retorna mensagem de sucesso
     }
-    }catch (err) {
-        console.error(err)
+    }catch (err) { // Catch para caso de erro
+        console.error(err) // Retorna no terminal qual foi o erro
 
-        return res.status(500).json({message: "Servidor instável, tente novamente mais tarde"})
+        return res.status(500).json({message: "Servidor instável, tente novamente mais tarde"}) // Mensagem de erro do servidor
     }
 
 })
 
-router.get('/listarTransformador', async (req, res) => {
-    const consult = await prisma.transformador.findMany()
+router.get('/listarTransformador', async (req, res) => { // Rota padrão para listar os transformadores no db
+    const consult = await prisma.transformador.findMany() // consult definida para buscar todos os transformadores no db
 
-    console.log(JSON.stringify(consult, null, 2))
+    console.log(JSON.stringify(consult, null, 2)) 
 
-    res.json(consult)
+    res.json(consult) // Transforma a resposta em json e retorna
 })
 
-router.delete('/deletarTransformador/:numeroDeSerie', async (req, res) => {
+router.delete('/deletarTransformador/:numeroDeSerie', async (req, res) => { // Rota padrão para deletar transformadores com numero de série
         const { numeroDeSerie } = req.params
 
         try {
@@ -59,7 +59,7 @@ router.delete('/deletarTransformador/:numeroDeSerie', async (req, res) => {
         }
     )
 
-router.put('/editarTransformador/:numeroDeSerie', async (req, res) => {
+router.put('/editarTransformador/:numeroDeSerie', async (req, res) => { // Rota padrão para editar transformadores com número de série
     const { numeroDeSerie } = req.params
     const data = req.body
 
@@ -74,6 +74,86 @@ router.put('/editarTransformador/:numeroDeSerie', async (req, res) => {
                 potenciaKva: data.potenciaKva,
                 statusProducao: data.statusProducao,
                 temperatura: data.temperatura
+            }
+        })
+        res.json(transformadorEdit)
+
+    } catch(err) {
+        console.log(err)
+
+        res.status(500).json({message: "Servidor instável, impossível alterar no momento"})
+    }
+})
+
+router.post('/registrarLog', async (req, res) => { // Rota padrão para registar logs de eventos dos transformadores
+    const log = req.body
+
+    try {
+        if (log.idTransformador === "") {
+            return res.status(500).json({message: "Campos inválidos, tente novamente"})
+        } else {
+        const newLog = await prisma.logEventos.create ( {
+        data: {
+            tipoEvento: log.tipoEvento,
+            descEvento: log.descEvento,
+            transformador: {
+                connect: {
+                idTransformador: log.idTransformador.trim()
+                }
+            }
+        }
+        })
+
+        return res.status(201).json({message:`Sucesso! Log registrado`}) 
+    }
+    }catch (err) {
+        console.error(err)
+
+        return res.status(500).json({message: "Servidor instável, tente novamente mais tarde"})
+    }
+
+})
+
+router.get('/listarLog', async (req, res) => { // Rota padrão para listar os logs de eventos dos transformadores
+    const consult = await prisma.logEventos.findMany()
+
+    console.log(JSON.stringify(consult, null, 2))
+
+    res.json(consult)
+})
+
+router.delete('/deletarLog/:idLog', async (req, res) => { // Rota padrão para deletar logs de eventos
+        const { idLog } = req.params
+
+        try {
+            const transformadorDeleted = await prisma.logEventos.delete ({
+                where: {
+                    idLog: idLog
+                }
+            })
+
+            res.json(transformadorDeleted)
+
+            } catch (err) {
+                console.error(err)
+
+                res.status(500).json({message: "Servidor instável, tente novamente mais tarde"})
+            }
+        }
+    )
+
+    router.put('/editarLog/:idLog', async (req, res) => { // rota padrão para editar logs de eventos
+    const { idLog } = req.params
+    const data = req.body
+
+    try {
+        const transformadorEdit = await prisma.logEventos.update ({
+            where: {
+                idLog: idLog
+            },
+            data: {
+                tipoEvento: data.tipoEvento,
+                descEvento: data.descEvento
             }
         })
         res.json(transformadorEdit)
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import 'dotenv/config'
-import express from 'express'
-import publicRouter from './routes/public.js'
+import 'dotenv/config' // Importando o .env
+import express from 'express' // Importando a biblioteca express
+import publicRouter from './routes/public.js' // Importando a pasta de rotas
 
-const port = process.env.PORT || 3000
-const app = express()
-app.use(express.json())
+const port = process.env.PORT || 3000 // Definindo porta dinâmica
+const app = express() // Definindo as funções do express para 'app'
+app.use(express.json()) // Definindo que express deve utilizar json
 
-app.use('/', publicRouter)
-app.get('/', (req, res) => {
+app.use('/', publicRouter) // Utilizando as rotas públicas
+app.get('/', (req, res) => { // Get só pra mostrar que tá rodando essa bomba 🔥🔥🔥
     res.send("Rodando essa bomba 🔥🔥🔥")
 })
 
-app.listen(port, () => {console.log("Rodando o servidor... 🔥")})
+app.listen(port, () => {console.log("Rodando o servidor... 🔥")}) // Utilizando a porta e exibindo mensagem que o servidor está rodando
